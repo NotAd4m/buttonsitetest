62e84dcb481ed46f1ba03299f96895af7a51760d
NotAd4m <148502805+NotAd4m@users.noreply.github.com>
2024-01-29 18:45:57 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,4 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    // Change 'newPage.html' to the path of the file you want to redirect to
+    window.location.href = 'some site.html';
+});
